f235015dfb43a7dbd1005689f336fcbba55bd629
MatthReich <matthreich@gmail.com>
2021-03-22 19:15:08 +0100

    begin with grid implementation

--- src/main/scala/Battleship/model/gridComponent/gridImplementation/Grid.scala
+++ src/main/scala/Battleship/model/gridComponent/gridImplementation/Grid.scala
@@ -1,8 +1,24 @@
 package Battleship.model.gridComponent.gridImplementation
 
 import Battleship.model.gridComponent.InterfaceGrid
+import Battleship.model.shipComponent.shipImplemenation.Ship
 
-case class Grid() extends InterfaceGrid {
+import scala.collection.mutable.ListBuffer
+
+class Grid(val size: Int, var listOfShips: ListBuffer[Ship]) extends InterfaceGrid {
+  private var field = Array.ofDim[Int](size, size)
 
 
+  def addShip(): Unit = {
+
+    update()
+  }
+
+  private def update(): Unit = {
+
+  }
+
+  def winStatement(): Boolean = {
+    listOfShips.nonEmpty && listOfShips.exists(_.status == false)
+  }
 }
--- src/test/scala/Battleship/model/gridComponent/GridSpec.scala
+++ src/test/scala/Battleship/model/gridComponent/GridSpec.scala
@@ -1,14 +1,26 @@
 package Battleship.model.gridComponent
 
+import Battleship.model.gridComponent.gridImplementation.Grid
+import Battleship.model.shipComponent.shipImplemenation.Ship
 import org.scalatest.wordspec.AnyWordSpec
+
+import scala.collection.mutable.ListBuffer
 
 class GridSpec extends AnyWordSpec {
 
+  val size = 10
+  val listOfShips = new ListBuffer[Ship]
+
   "A Grid" when {
+
+    val grid = new Grid(size, listOfShips)
 
     "new" should {
       "have no ships" in {
-
+        assert(grid.listOfShips.isEmpty)
+      }
+      "say that game is not finished" in {
+        assert(!grid.winStatement())
       }
     }
 
@@ -16,10 +28,19 @@ class GridSpec extends AnyWordSpec {
       "change grid values" in {
 
       }
+      "not allow a ship with coordinates already exists" in {
+
+      }
     }
 
     "a ship gets hit" should {
       "change grid values" in {
+
+      }
+    }
+
+    "game is finished" should {
+      "say game is finished" in {
 
       }
     }
