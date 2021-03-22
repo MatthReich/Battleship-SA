09d4c62aec5619bb5813640f6c0316bde2c2a4a9
MatthReich <matthreich@gmail.com>
2021-03-22 18:43:20 +0100

    added first thoughts to grid

--- src/test/scala/Battleship/model/gridComponent/GridSpec.scala
+++ src/test/scala/Battleship/model/gridComponent/GridSpec.scala
@@ -4,4 +4,25 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class GridSpec extends AnyWordSpec {
 
+  "A Grid" when {
+
+    "new" should {
+      "have no ships" in {
+
+      }
+    }
+
+    "ship get setted" should {
+      "change grid values" in {
+
+      }
+    }
+
+    "a ship gets hit" should {
+      "change grid values" in {
+
+      }
+    }
+  }
+
 }
