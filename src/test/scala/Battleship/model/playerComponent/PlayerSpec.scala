5b1d29d36e790e2208b49b531f7798f3d605d2c8
MatthReich <matthreich@gmail.com>
2021-03-29 16:22:04 +0200

    player test added

---
+++ src/test/scala/Battleship/model/playerComponent/PlayerSpec.scala
@@ -0,0 +1,69 @@
+package Battleship.model.playerComponent
+
+import Battleship.controller.controllerbaseimpl.GameState
+import Battleship.model.gridComponent.InterfaceGrid
+import Battleship.model.gridComponent.gridImplementation.Grid
+import Battleship.model.gridComponent.strategyCollide.StrategyCollideNormal
+import Battleship.model.playerComponent.playerImplementation.Player
+import Battleship.model.shipComponent.InterfaceShip
+import Battleship.model.shipComponent.shipImplemenation.Ship
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+class PlayerSpec extends AnyWordSpec {
+
+  val name: String = "Matthias"
+  val newName: String = "Marcel"
+  val shipList = new ListBuffer[InterfaceShip]
+  val grid: InterfaceGrid = Grid(10, new StrategyCollideNormal, new Array[mutable.Map[String, Int]](0)).initGrid()
+  val shipLength = 3
+  val shipArray: Array[scala.collection.mutable.Map[String, Int]] = Array(
+    scala.collection.mutable.Map("x" -> 0, "y" -> 0, "value" -> 1),
+    scala.collection.mutable.Map("x" -> 0, "y" -> 1, "value" -> 1),
+    scala.collection.mutable.Map("x" -> 0, "y" -> 2, "value" -> 1)
+  )
+  val status = false
+  val ship: InterfaceShip = Ship(shipLength, shipArray, status)
+
+
+  "A Player" when {
+
+    var player: InterfacePlayer = Player(name, shipList, grid)
+
+    "new" should {
+      "have a name" in {
+        assert(player.name === name)
+      }
+      "have an empty List" in {
+        assert(player.shipList.isEmpty === true)
+      }
+      "have an grid with no ships in it" in {
+        assert(player.grid.grid.exists(_.get("value").contains(1)) === false)
+      }
+    }
+    "name gets updated" should {
+      "change name" in {
+        player = player.updateName(newName)
+        assert(player.name === newName)
+      }
+    }
+    "grid gets updated" should {
+      "update grid" in {
+        val newGrid = grid.setField(GameState.SHIPSETTING, Array(mutable.Map("x" -> 0, "y" -> 0)))
+        assert(newGrid._2 === true)
+        player = player.updateGrid(newGrid._1)
+        assert(player.grid.grid(0).getOrElse("value", Int.MaxValue) === 1)
+      }
+    }
+    "ship gets added" should {
+      "extend List of ships" in {
+        player = player.updateShip(ship)
+        assert(player.shipList.nonEmpty === true)
+        assert(player.shipList.length === 1)
+      }
+    }
+  }
+
+}
