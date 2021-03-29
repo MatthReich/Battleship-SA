bbf6530a89729b7bb6175e3bee62ce5718dca920
Marcelg1235 <Marcelg1235@gmail.com>
2021-03-29 15:18:58 +0200

    Add Coords in Controller

--- src/main/scala/Battleship/controller/controllerbaseimpl/Controller.scala
+++ src/main/scala/Battleship/controller/controllerbaseimpl/Controller.scala
@@ -5,9 +5,8 @@ import Battleship.controller.controllerbaseimpl.GameState.{GameState, PLAYERSETT
 import Battleship.controller.controllerbaseimpl.PlayerState.{PLAYER_ONE, PLAYER_TWO, PlayerState}
 import Battleship.model.playerComponent.InterfacePlayer
 import Battleship.model.shipComponent.shipImplemenation.Ship
+
 import scala.collection.mutable
-
-
 import scala.swing.Publisher
 
 case class Controller(var player_01: InterfacePlayer, var player_02: InterfacePlayer, gameState: GameState, playerState: PlayerState) extends InterfaceController with Publisher {
@@ -15,7 +14,7 @@ case class Controller(var player_01: InterfacePlayer, var player_02: InterfacePl
   val shipNotSunk = false
 
   override def setShip(input: String): Unit = {
-    val coords: Array[mutable.Map[String, Int]] = calculateCoords()
+    val coords: Array[mutable.Map[String, Int]] = calculateCoords(input)
 
     val functionHelper = changePlayerStats(coords) _
     playerState match {
@@ -52,8 +51,40 @@ case class Controller(var player_01: InterfacePlayer, var player_02: InterfacePl
     }
   }
 
-  private def calculateCoords(): Array[mutable.Map[String, Int]] = {
-    Array(mutable.Map("x" -> 0))
+  private def calculateCoords(input: String): Array[mutable.Map[String, Int]] = {
+    val splittedInput = input.split(" ")
+    if (splittedInput.length == 4) {
+      val splittedInputInt = splittedInput.map(_.toInt)
+      if (checkShipFormat(splittedInputInt)) {
+        val tmpArray = new Array[mutable.Map[String, Int]](getShipSize(splittedInputInt))
+        var i = 0
+        for (x <- splittedInputInt(0) to splittedInputInt(2)) {
+          for (y <- splittedInputInt(1) to splittedInputInt(3)) {
+            tmpArray(i) = mutable.Map("x" -> x, "y" -> y)
+            i += 1
+          }
+        }
+        tmpArray
+      }
+    }
+    null
+  }
+
+  private def getShipSize(splittedInputInt: Array[Int]): Int = {
+    if (splittedInputInt(0) == splittedInputInt(2)) {
+      math.max(calcDiff(splittedInputInt(1), splittedInputInt(3)), calcDiff(splittedInputInt(3), splittedInputInt(1)))
+    } else {
+      math.max(calcDiff(splittedInputInt(0), splittedInputInt(2)), calcDiff(splittedInputInt(2), splittedInputInt(0)))
+    }
+  }
+
+  private def calcDiff(nr1: Int, nr2: Int): Int = {
+    (nr1 - nr2 + 1)
+  }
+
+  private def checkShipFormat(splittedInputInt: Array[Int]): Boolean = {
+    ((splittedInputInt(0) == splittedInputInt(2) && splittedInputInt(1) == splittedInputInt(3))
+      || (!(splittedInputInt(0) == splittedInputInt(2)) && !(splittedInputInt(1) == splittedInputInt(3))))
   }
 
   private def getSize(coords: Array[mutable.Map[String, Int]]): Int = coords.length
