37e90b5418769ed735fef65e5501a3636bdc8136
MatthReich <matthreich@gmail.com>
2021-05-03 16:48:21 +0200

    Changes by Marcel

--- Model/src/main/scala/Battleship/model/databaseComponent/slick/ShipCoordinatesTable.scala
+++ Model/src/main/scala/Battleship/model/databaseComponent/slick/ShipCoordinatesTable.scala
@@ -2,11 +2,13 @@ package Battleship.model.databaseComponent.slick
 
 import slick.jdbc.MySQLProfile.api._
 
-class ShipCoordinatesTable(tag: Tag) extends Table[(Int, Int, Int)](tag, "ShipCoordinatesTable") {
+class ShipCoordinatesTable(tag: Tag) extends Table[(Int, Int, Int, Int)](tag, "ShipCoordinatesTable") {
 
-  override def * = (id, x_value, y_value)
+  override def * = (id, playerId,x_value, y_value)
 
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+
+  def playerId = column[Int]("playerId")
 
   def x_value = column[Int]("XValue")
 
